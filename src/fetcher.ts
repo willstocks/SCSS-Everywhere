c99aacc25fa9b271eaff54c2aebd6cd59ccb9a67
Gencer W. Genç <gencer@nienbo.co.uk>
2018-08-16 16:26:56 +0300

    Added ability to handle remote CSS files. This closes #1.

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,5 +1,12 @@
 import * as vscode from "vscode";
+import * as os from 'os';
+import * as fs from 'fs';
+import * as http from 'http';
+import * as https from 'https';
 import ParseEngineRegistry from "./parse-engines/parse-engine-registry";
+import { URL } from "url";
+import * as crypto from 'crypto';
+import * as path from 'path';
 
 class Fetcher {
     public static async findAllParseableDocuments(): Promise<vscode.Uri[]> {
@@ -14,8 +21,54 @@ class Fetcher {
         const configuration = vscode.workspace.getConfiguration();
         const includeGlobPattern = configuration.get("html-css-class-completion.includeGlobPattern");
         const excludeGlobPattern = configuration.get("html-css-class-completion.excludeGlobPattern");
+        const remoteStyleSheets = configuration.get<Array<string>>("html-css-class-completion.remoteStyleSheets");
 
-        return await vscode.workspace.findFiles(`${includeGlobPattern}`, `${excludeGlobPattern}`);
+        var paths;
+        if (remoteStyleSheets.length > 0) {
+            const folder = path.join(os.tmpdir(), "html_css_slim");
+            if (!fs.existsSync(folder)){
+                fs.mkdirSync(folder);
+            }
+            for (let remoteFile of remoteStyleSheets) {
+                const filename = this.getFilename(remoteFile);
+                const url = new URL(remoteFile);
+                if (url.protocol == 'https:') {
+                    https.get({
+                        host: url.host,
+                        path: url.pathname,
+                        method: 'GET',
+                        port: 443
+                    }, function (response) {
+                        var file = fs.createWriteStream(path.join(folder, filename));
+                        response.pipe(file);
+                    });
+                } else {
+                    http.get(remoteFile, function (response) {
+                        var file = fs.createWriteStream(path.join(folder, filename));
+                        response.pipe(file);
+                    });
+                }
+            }
+
+            const relativePattern = new vscode.RelativePattern(folder, '*.css');
+            paths = await vscode.workspace.findFiles(relativePattern);
+            var localFiles = await vscode.workspace.findFiles(`${includeGlobPattern}`, `${excludeGlobPattern}`);
+    
+            for (let parsedFile of paths) {
+                localFiles[localFiles.length] = parsedFile
+            }
+        }
+
+        return localFiles;
+    }
+
+    // Parse Filename from URL if not found (such as ends with folder) then md5(url) returns
+    private static getFilename(url: string): string {
+        const filename = decodeURIComponent(new URL(url).pathname.split('/').pop());
+        if (!filename) {
+            return crypto.createHash('md5').update(url).digest("hex");
+        }
+        return filename;
     }
 }
 
